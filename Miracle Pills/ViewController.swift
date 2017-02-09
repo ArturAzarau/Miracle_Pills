481e3213c857d21ec3c8c26be1288d8c54ed2629
Artur Azarau <artur.azarau@gmail.com>
2017-02-09 20:02:20 +0300

    Initial Commit

---
+++ Miracle Pills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Miracle Pills
+//
+//  Created by Artur Azarov on 09.02.17.
+//  Copyright © 2017 Artur Azarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
